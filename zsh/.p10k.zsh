91bfcddb1c1538913da42c2a7cee38eebff8dd89
Ollz-png <OllzMakesStuff@gmail.com>
2024-11-09 13:04:50 +0000

    Auto commit

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -34,11 +34,13 @@
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
     os_icon                 # os identifier
+    user
     dir                     # current directory
-    vcs                     # git status
+    vcs
+    battery                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
-    # prompt_char           # prompt symbol
+    prompt_char           # prompt symbol
   )
 
   # The list of segments shown on the right. Fill it with less important segments.
